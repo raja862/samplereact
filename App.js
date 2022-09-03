e71b0ce025bc08bca2059931e710e07479a085f7
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2022-09-03 18:13:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Roo(props){
+  let ram=23*33
+  return(<>
+  
+  
+  <h1>
+    Hello India<br></br>
+    My New Car is {props.carBrand} cartype {props.carType} and super mileage is {props.carMileage}<br></br>
+   the ans is:{ram}
+  </h1>
+  
+  </>
+  )
+}
+export default Roo;
